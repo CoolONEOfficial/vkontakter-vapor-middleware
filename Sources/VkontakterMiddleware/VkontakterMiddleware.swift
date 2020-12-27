b00775c2fde1d938900be441f0970f25162c345a
coolone <coolone.official@gmail.com>
2020-12-27 21:27:47 +0300

    Remove setWh code to Vkontakter

--- Sources/VkontakterMiddleware/VkontakterMiddleware.swift
+++ Sources/VkontakterMiddleware/VkontakterMiddleware.swift
@@ -23,87 +23,20 @@ public extension VkontakterMiddleware {
             request.logger.critical("Received empty request from Vk Server")
             return next.respond(to: request)
         }
+        
+        let update = dispatcher.enqueue(bytebuffer: body)
 
-        if let code = bot.confirmationCode {
+        let bodyContent: String
+        if update?.type == .confirmation, let code = bot.confirmationCode {
             bot.confirmationCode = nil
-            debugPrint("Returning confirmationCode")
-            return request.eventLoop.makeSucceededFuture(Response(
-                status: .ok, version: request.version,
-                headers: request.headers, body: .init(string: code)
-            ))
+            request.logger.debug(.init(stringLiteral: "Returning confirmationCode"))
+            bodyContent = code
+        } else {
+            bodyContent = "ok"
         }
-        
-        dispatcher.enqueue(bytebuffer: body)
-        
-        return request.eventLoop.makeSucceededFuture(Response.init(status: .ok, version: request.version, headers: .init(), body: .init(staticString: "ok")))
+
+        return request.eventLoop.makeSucceededFuture(Response.init(status: .ok, version: request.version, headers: .init(), body: .init(string: bodyContent)))
     }
     
-    private func updateConfirmationCode(_ groupId: UInt64) throws -> EventLoopFuture<Bot.GetCallbackConfirmationCodeResp> {
-        try bot.getCallbackConfirmationCode(params: .init(groupId: groupId)).flatMapThrowing { resp in
-            bot.confirmationCode = resp.code
-            return resp
-        }
-    }
     
-    private func createServer(_ groupId: UInt64, _ serverUrl: String, serverName: String?, secretKey: String) throws -> EventLoopFuture<Bot.AddCallbackServerResp> {
-        var serverName = serverName
-        if serverName == nil {
-            print("Enter name for new VK callback API server: ")
-            serverName = readLine()!
-        }
-
-        return try bot.addCallbackServer(params: .init(
-            groupId: groupId, url: serverUrl,
-            title: serverName!, secretKey: secretKey
-        ))
-    }
-    
-    private func setServerSettings(_ groupId: UInt64, _ serverUrl: String, _ serverId: UInt64) throws -> EventLoopFuture<VkFlag> {
-        try bot.setCallbackSettings(params: .init(
-            groupId: groupId, serverId: serverId,
-            apiVersion: "5.126", messageNew: .on
-        ))
-    }
-
-    func setWebhooks(serverName: String?) throws -> EventLoopFuture<Void> {
-        guard let config = bot.settings.webhooksConfig, let groupId = config.groupId else {
-            throw CoreError(
-                type: .internal,
-                reason: "Initialization parameters (with groupId) wasn't found in enviroment variables"
-            )
-        }
-
-        return try bot.getCallbackServers(params: .init(groupId: groupId)).flatMapThrowing { serversResp in
-            let servers = serversResp.items
-            let serverUrl = config.url
-
-            let allSteps: (() throws -> Void) = {
-                try updateConfirmationCode(groupId).flatMapThrowing { resp in
-                    let secretKey: String = .random(ofLength: 15)
-
-                    try createServer(groupId, serverUrl, serverName: serverName, secretKey: secretKey).flatMapThrowing { resp in
-                        bot.setSecretKey(secretKey)
-
-                        try setServerSettings(groupId, serverUrl, resp.serverId)
-                    }
-                }
-            }
-
-            if let matchServer = servers.first(where: { $0.url == serverUrl }) {
-                if matchServer.status != .ok {
-                    debugPrint("Server founded but status is \(matchServer.status!.rawValue) on Callback API")
-                    try bot.deleteCallbackServer(params: .init(groupId: groupId, serverId: matchServer.id!)).flatMapThrowing { flag in
-                        assert(flag.bool)
-                        try allSteps()
-                    }
-                } else {
-                    bot.setSecretKey(matchServer.secretKey!)
-                    debugPrint("Server already configured on Callback API")
-                }
-            } else {
-                debugPrint("Server not found on Callback API")
-                try allSteps()
-            }
-        }
-    }
 }
