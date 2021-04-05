76d8b5a797dc9b03130c9ac8ee3fac604a4627f5
coolone <coolone.official@gmail.com>
2021-04-05 20:28:52 +0300

    Fix vkontakter dep

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.9.0"),
-        .package(url: "https://github.com/CoolONEOfficial/Vkontakter.git", .branch("master")),
+        .package(url: "https://github.com/CoolONEOfficial/Vkontakter.git", from: "0.1.0"),
     ],
     targets: [
         .target(
